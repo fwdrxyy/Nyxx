27c6338c6428fb53bc2124e8b89d63191281b3e3
Gabe <128531622+fwdrxyy@users.noreply.github.com>
2026-02-06 09:13:32 -0500

    change Logging & the main file

--- Cogs/Logging.py
+++ Cogs/Logging.py
@@ -3,6 +3,10 @@ import random
 from discord.ext import commands
 from datetime import datetime
 
+intents = discord.Intents.default() 
+intents.message_content = True
+intents.guild_messages = True # REQUIRED for Py‑Cord 
+
 class Logging(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
--- nyxx.py
+++ nyxx.py
@@ -2,9 +2,11 @@ import discord
 from discord.ext import commands, tasks
 import asyncio
 import os
-from dotenv import load_dotenv
+"from dotenv import load_dotenv"
 
 intents = discord.Intents.default()
+intents.message_content = True
+intents.guild_messages = True
 intents.members = True
 
 bot = commands.Bot(intents=intents)
@@ -20,16 +22,16 @@ async def on_ready():
     
 @tasks.loop(seconds=10)  # Change status every 10 seconds
 async def change_status():
-    await bot.wait_until_ready()  # Ensure the bot is ready
+    await bot.wait_until_ready()  # Ensure nyxx is ready
     while True:
         server_count = len(bot.guilds)
         statuses = [
             discord.Game(name="Now Online via Visual Studio Code"),
             discord.Game(name="Helping people with Homebrew issues"),
-            discord.Game(name=f"Watching {server_count} servers!"),
-            discord.Game(name="discord.gg/QQXVaFbD4K"),
+            discord.Game(name=f"Watching {server_count} servers carefully..."),
+            discord.Game(name="discord.gg/Cc6q3BgTSR"),
             discord.Game(name="youtube.com/@fwdrxyy_"),
-            discord.Game(name="fwdrxyy.github.io/")
+            discord.Game(name="fwdrxyy.github.io/"),
             discord.Game(name="New Feature! ModMail System"),
         ]
         for status in statuses:
